d3afd3d130b3b87b4b2a6f285ea4cdbf8a1c1c85
vs0uz4 <vitor.rodrigues@gmail.com>
2018-12-06 20:44:41 -0200

    Assets and Styles Extracted from HomeScreen and Added in Separate Files.

---
+++ src/screens/HomeScreen/assets.js
@@ -0,0 +1,9 @@
+const assets = {
+  background: require('../../../assets/background.png'),
+  tripplanner: require('../../../assets/logo_tripplanner.png'),
+  devpleno: require('../../../assets/logo_dev_pleno.png'),
+  arrow: require('../../../assets/arrow_rigth.png'),
+  pin: require('../../../assets/pin.png')
+}
+
+export default assets
---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import { View, Text, ImageBackground, Image, TouchableWithoutFeedback } from 'react-native'
+import assets from './assets'
+import styles from './styles'
+
+class HomeScreen extends Component {
+  state = {
+    show: true
+  }
+
+  handleShow = () => {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+
+  render () {
+    return (
+      <ImageBackground
+        source={assets.background}
+        imageStyle={{ resizeMode: 'stretch' }}
+        style={styles.background}
+      >
+        <View style={styles.wrapperLogoTripPlanner}>
+          <Image source={assets.tripplanner} />
+        </View>
+
+        <View style={styles.wrapperLogoDevPleno}>
+          <Image source={assets.devpleno} />
+        </View>
+        {
+          !this.state.show
+            ? <TouchableWithoutFeedback onPress={this.handleShow}>
+              <View style={styles.buttonBackground}>
+                <Text style={styles.buttonText}>COMEÇAR</Text>
+              </View>
+            </TouchableWithoutFeedback>
+            : <TouchableWithoutFeedback onPress={this.handleShow}>
+              <View style={styles.buttonEmptyStateBackground}>
+                <Image source={assets.pin} style={styles.pin} />
+                <Text style={styles.buttonEmptyStateText}>Vamos planejar sua primeira viagem?</Text>
+                <Image source={assets.arrow} style={styles.arrow} />
+              </View>
+            </TouchableWithoutFeedback>
+        }
+
+      </ImageBackground>
+    )
+  }
+}
+
+export default HomeScreen
