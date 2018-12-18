2016d723483a0a123534458d5df5c774f439c406
vs0uz4 <vitor.rodrigues@gmail.com>
2018-12-18 13:04:37 -0200

    Installed and Configured 'react-native-image-picker'

--- android/app/src/main/java/com/tripplaner/MainApplication.java
+++ android/app/src/main/java/com/tripplaner/MainApplication.java
@@ -3,6 +3,7 @@ package com.tripplaner;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new MapsPackage(),
             new RNDeviceInfo(),
             new RNGestureHandlerPackage()
